9a7f06e0370988c0223c8fd291d519d3fe7e1920
hugo <armandogarciahdz37@gmail.com>
2024-02-15 17:56:19 -0600

    agregando el campo telefono a la tabla usuario

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def registro(request):
-    return render(request,'registro.html')
+    
+    if request.method == 'GET':
+        return render(request,'registro.html',{
+        'form': UserCreationForm
+    })
+    else:
+        print(request)
+        
+    
+    
 
 def login(request):
     return render(request,'login.html')
