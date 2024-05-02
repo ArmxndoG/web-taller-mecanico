d1e23a66bab521ef53d5649d80f61abf4df1bae0
JoseJ <jjdelangel2001@gmail.com>
2024-05-01 20:17:05 -0600

    opciones de hora que se muestre de 9 a 11 y validacion de no mas de 5 citas por dia

--- autenticacion/forms.py
+++ autenticacion/forms.py
@@ -1,23 +1,30 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .models import User,Cita,Servicio,DetalleCita, ImagenFase
+from django.forms.widgets import SelectDateWidget
+from datetime import date
+from django.utils.html import format_html
+from django.db import models  # Agrega esta importación
 
 class RegistrarUsuario(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name','last_name','email','tel','username','password1','password2']
 
-    
-   
+class CustomDateInput(forms.DateInput):
+    def __init__(self, attrs=None):
+        today = date.today()
+        super().__init__(attrs={'type': 'date', 'min': today.strftime('%Y-%m-%d')})
+
+
 class CitaForm(forms.ModelForm):
     class Meta:
         model = Cita
-        fields = ['modelo_carro', 'color', 'placas', 'fecha', 'hora','servicios']
+        fields = ['modelo_carro', 'color', 'placas', 'fecha', 'hora', 'servicios']
         
         widgets = {
-            'fecha': forms.DateInput(attrs={'type': 'date'}),
-            'hora': forms.TimeInput(attrs={'type': 'time'}),
-            
+            'fecha': CustomDateInput(),
+            'hora': forms.Select(choices=[('09:00', '9:00'), ('10:00', '10:00'), ('11:00', '11:00')]),
         }
         
     servicios = forms.ModelMultipleChoiceField(
@@ -27,10 +34,25 @@ class CitaForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Inicializar queryset para el campo de servicios
         self.fields['servicios'].queryset = Servicio.objects.all()
-    
 
+    def clean_fecha(self):
+        fecha = self.cleaned_data['fecha']
+        citas_del_dia = Cita.objects.filter(fecha=fecha).count()
+        if citas_del_dia >= 5:
+            raise forms.ValidationError("No se pueden agendar más de 5 citas para este día.")
+        return fecha
+
+    def clean(self):
+        cleaned_data = super().clean()
+        fecha = cleaned_data.get("fecha")
+        hora = cleaned_data.get("hora")
+        if fecha:
+            citas_del_dia = Cita.objects.filter(fecha=fecha).count()
+            if citas_del_dia >= 5:
+                self.add_error('fecha', "No se pueden agendar más de 5 citas para este día.")
+        return cleaned_data
+        
 class ImagenForm(forms.ModelForm):
     class Meta:
         model = ImagenFase
