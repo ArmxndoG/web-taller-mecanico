2a21a55feef0650239297d7a4a1f262170b194b3
hugo <armandogarciahdz37@gmail.com>
2024-05-18 14:50:46 -0600

    rol de administrador en el panel de django admin

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from autenticacion.models import User,Cita,Servicio,Fase
+from autenticacion.models import User,Cita,Servicio,Fase,DetalleCita,ImagenFase
 # Register your models here.
 
+admin.site.register(User)
 admin.site.register(Cita)
 admin.site.register(Servicio)
 admin.site.register(Fase)
+admin.site.register(DetalleCita)
+admin.site.register(ImagenFase)
 
 
 
